065fe38809175cdfbb77e25df464a68353ff4a7b
28JayG <jaygandhi.32@outlook.com>
2022-08-05 18:09:20 +0530

    values for DB shifted to env

--- queries.js
+++ queries.js
@@ -3,9 +3,9 @@ const Pool = require("pg").Pool;
 const pool = new Pool({
   user: process.env.PSQL_USER,
   password: process.env.PSQL_PASSWORD,
-  host: "localhost",
+  host: process.env.PSQL_HOST,
   database: process.env.PSQL_DB,
-  port: 5432,
+  port: process.env.PSQL_PORT,
 });
 
 module.exports = pool;
