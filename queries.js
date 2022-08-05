f2438b298e3bbb19bca1a2565e73202f0357d27b
28JayG <jaygandhi.32@outlook.com>
2022-08-05 18:25:16 +0530

    adds ssl connect to pool

--- queries.js
+++ queries.js
@@ -1,13 +1,14 @@
 const Pool = require("pg").Pool;
 
-console.log(process.env.PSQL_HOST)
-
 const pool = new Pool({
   user: process.env.PSQL_USER,
   password: process.env.PSQL_PASSWORD,
   host: process.env.PSQL_HOST,
   database: process.env.PSQL_DB,
   port: process.env.PSQL_PORT,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
